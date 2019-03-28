6cf22275d70bf911a94ce066b63c2901e435e2a6
Christian <aramaki2501@gmail.com>
2019-03-28 00:36:08 +0000

    Addes Residual reg, residual boxplot, residual qqplot.

--- app.R
+++ app.R
@@ -12,6 +12,7 @@ library(shiny)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
+
    # Application title
    titlePanel("Regresión lineal simple"),
 
@@ -20,15 +21,27 @@ ui <- fluidPage(
       sidebarPanel(
         textInput("vx",
                   "Valores de x:",
-                  value = paste(1:10, collapse=', ')),
+                  value = paste(1:10, collapse = ', ')),
         textInput("vy",
                   "Valores de y:",
-                  value = paste((1:10)*2+round(runif(10,0,1),2), collapse=', '))
+                  value = paste((1:10)*2 + round(runif(10,0,1),2), collapse = ', '))
       ),
 
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+        tabsetPanel(
+          tabPanel('Gráfica Regresión',plotOutput('regplot'),
+                   hr(),
+
+                   uiOutput('formula')),
+          tabPanel('Gráfica Errores',plotOutput('errplot')),
+
+          tabPanel('Boxplot Errores',plotOutput('errboxplot')),
+
+          tabPanel('QQ-Plot Errores',plotOutput('errqqplot'))
+
+
+        )
       )
    )
 )
@@ -57,25 +70,67 @@ server <- function(input, output) {
       str_split(",", simplify = TRUE) %>%
       map(conversor) %>%
       unlist()
-  })-> x
+  }) -> x
 
   reactive({
     input$vy %>%
       str_split(",", simplify = TRUE) %>%
       map(conversor) %>%
       unlist()
-  })-> y
+  }) -> y
 
-   output$distPlot <- renderPlot({
+  data <- reactive({data.frame(x = x(), y = y())})
+  lmfit    <- reactive({lm(y ~ x, data = data())})
+
+   output$regplot <- renderPlot({
       # generate bins based on input$bins from ui.R
-     data <- reactive({data.frame(x=x(), y=y())})
-     lmfit    <- reactive({lm(y ~ x, data = data())})
+
 
 
 
       ggplot(data(), aes(x = x, y = y)) +
-        geom_point(size=3) +
-        stat_smooth(method = "lm", col = "red", se = FALSE)
+        geom_point(size = 3) +
+        stat_smooth(method = "lm", col = "blue", se = FALSE)
+   })
+
+   output$formula <- renderUI({
+     withMathJax(paste0('$$\\hat{y}=',round(lmfit()$coefficients[1],4),'+',
+                        round(lmfit()$coefficients[2],4),'x;\\qquad R^2=',round(summary(lmfit())$r.squared*100,2),'\\%$$'))
+
+   })
+
+   output$errplot <- renderPlot({
+     # generate bins based on input$bins from ui.R
+
+
+
+     datar <- reactive({data.frame(x = x(), y = lmfit()$residuals)})
+     ggplot(datar(), aes(x = x, y = y)) +
+       geom_point(size = 3) +
+       stat_smooth(method = "lm", col = "blue", se = FALSE)
+   })
+
+   output$errboxplot <- renderPlot({
+     # generate bins based on input$bins from ui.R
+
+
+
+     datab <- reactive({data.frame(y = lmfit()$residuals)})
+     ggplot(data = datab(), aes(x = '', y = y)) +
+       geom_boxplot(fill = "#4271AE", colour = "#1F3552", alpha = 0.7) +
+       scale_y_continuous(name = "residuals")
+   })
+
+
+   output$errqqplot <- renderPlot({
+     # generate bins based on input$bins from ui.R
+
+
+
+     datab <- reactive({data.frame(y = lmfit()$residuals)})
+     ggplot(data = datab(), aes(sample = y)) +
+       stat_qq() +
+       stat_qq_line()
    })
 }
 
